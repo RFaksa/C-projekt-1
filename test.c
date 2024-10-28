d777682b33c182aec248508bbd5768f69b9f9169
RFaksa <99041185+RFaksa@users.noreply.github.com>
2024-10-28 21:30:23 +0100

    Update test.c

--- test.c
+++ test.c
@@ -20,11 +20,12 @@ void dotprod_function(char filePath[256]);
 
 int main() {
     char funkce[256];
-    printf("Jakou funkci chces pouzit \n (sqrt/mandelbrot/dotprod [umisteni souboru]): \n");
+    printf("Jakou funkci chces pouzit \n (sqrt/mandelbrot/dotprod): \n");
 
     fgets(funkce, sizeof(funkce), stdin);
     funkce[strcspn(funkce, "\n")] = 0;
 
+    //tohle je maximalne nechutne reseni ale funguje to takze asi whatever :D
     if (strcmp(funkce, "sqrt") == 0) {
         sqrt_function();
     } else if (strcmp(funkce, "mandelbrot") == 0) {
@@ -51,8 +52,8 @@ void mandelbrot_function() {
 
 
 void dotprod_function(char filePath[256]) {
-    char line1[256], line2[256], *token;
-    double vector1[256], vector2[256], soucin; //maximalni pocet prvků ve vektoru
+    char line1[512], line2[512], token;
+    double vector1[256], vector2[256], soucin; //maximalni pocet prvků ve vektoru je 256 (v pripade zvetsit)
     int Vectory1, Vectory2, i;
 
     FILE *file;
@@ -63,15 +64,15 @@ void dotprod_function(char filePath[256]) {
 
     file = fopen(filePath, "r");
     if (file == NULL) {
-        fprintf(stderr, "Chyba při otevírání souboru");
+        fprintf(stderr, "Chyba pri otevirani souboru");
     }
 
     if (fgets(line1, sizeof(line1), file) == NULL) {
-        fprintf(stderr, "Chyba při čtení prvního řádku");
+        fprintf(stderr, "Chyba pri cteni prvniho radku");
         fclose(file);
     }
     if (fgets(line2, sizeof(line2), file) == NULL) {
-        fprintf(stderr, "Chyba při čtení druhého řádku");
+        fprintf(stderr, "Chyba pri cteni druheho radku");
         fclose(file);
     }
 
@@ -99,7 +100,7 @@ void dotprod_function(char filePath[256]) {
     Vectory2 = i;
 
     if (Vectory1 != Vectory2) {
-        fprintf(stderr, "Chyba: Vektory mají různé délky\n");
+        fprintf(stderr, "Vektory maji ruzne delky, nelze vypocitat\n");
     }
 
     i = 0;
